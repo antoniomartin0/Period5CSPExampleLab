839bcd641954f0ca436075864e345214d8b0c214
Antonio Martin <antonio.martin@vtsd.com>
2022-09-09 12:27:27 -0400

    Data Type Update

---
+++ DataTypes.java
@@ -0,0 +1,15 @@
+// Write whatever I want !@#$%^&*()
+/*
+    Multi-Line comment syntax.
+
+    Nothing in here will be executed (ran)
+
+ */
+public class DataTypes {
+
+    // Variable syntax: dataType varName = value;
+    int FavInt = 5;
+    double snicker = 7.5;
+    boolean likeDogs = true;
+
+}
--- FirstProgram.java
+++ FirstProgram.java
@@ -1,3 +1,10 @@
 public class FirstProgram {
+    public static void main(String args[]){
+
+        System.out.print("Hello World");
+
+    }
+
+
+
 }
-Class
