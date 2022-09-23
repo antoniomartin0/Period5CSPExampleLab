a638a706910f661f88ea7b5b63cca72afcac8138
Antonio Martin <antonio.martin@vtsd.com>
2022-09-23 13:15:41 -0400

    Data Type Update

--- DataTypes.java
+++ DataTypes.java
@@ -6,10 +6,19 @@
 
  */
 public class DataTypes {
+    public static void main(String args[]){
+        int FavInt = 5;
+        double snicker = 7.5;
+        boolean likeDogs = true;
 
-    // Variable syntax: dataType varName = value;
-    int FavInt = 5;
-    double snicker = 7.5;
-    boolean likeDogs = true;
+        System.out.println("Favorite Integer is:" + FavInt + "and the class snicker was" + snicker);
+
+        int n1 = 1;
+        int n2 = 6;
+
+        System.out.print(n1 + n2 / n1);
+
+
+    }
 
 }
---
+++ Lab03.java
@@ -0,0 +1,17 @@
+public class Lab03 {
+    public static void main(String args[])
+    {
+        System.out.println("Lab03, 80 Point Version\n");
+
+        int startingValue = 10000;
+        int hours = startingValue / 3600;
+        int seconds = startingValue % 3600;
+        int minutes = seconds / 60;
+        int los = seconds % 60;
+
+
+
+
+    }
+
+}
