30a646a24fe32ce722731e35f46e1a593275c471
Antonio Martin <113053094+antoniomartin0@users.noreply.github.com>
2022-10-24 06:02:03 -0400

    Update Lab03.java

--- Lab03.java
+++ Lab03.java
@@ -1,17 +1,7 @@
-public class Lab03 {
-    public static void main(String args[])
-    {
-        System.out.println("Lab03, 80 Point Version\n");
-
-        int startingValue = 10000;
-        int hours = startingValue / 3600;
-        int seconds = startingValue % 3600;
-        int minutes = seconds / 60;
-        int los = seconds % 60;
-
-
-
-
-    }
-
-}
+// Write a loop that outputs the numbers from 100 to 1 by 3's
+        // Ex: 100, 97, 94, 91, 88, ...
+        for(int k=0; k <= 100; k+=3){
+            System.out.print(k + ", ");
+            for (int k = 1; k <= 100; k++)
+            System.out.print(p + " ");
+        }
